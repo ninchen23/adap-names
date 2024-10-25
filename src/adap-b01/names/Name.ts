6700d8af8bfa88a7dd592ad0a4e66b3131a923e9
Nina Mücke <ninamuecke@gmx.com>
2024-10-25 13:12:23 +0200

    implement name.ts

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -6,36 +6,64 @@ export class Name {
     private components: string[] = [];
     private delimiter: string = this.DEFAULT_DELIMITER;
 
+    // @methodtype initialization-method
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
+        if (delimiter !== undefined) {
+            this.delimiter = delimiter;
+        }
+        this.components = [...other];
     }
 
+    // @methodtype conversion-method
     public asNameString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        let nameString: string = "";
+        this.components.forEach((c, i) => {
+            nameString += c;
+            if (i < this.getNoComponents() - 1) {
+                nameString += delimiter;
+            }
+        })
+        return nameString
     }
 
+    // @methodtype get-method
     public getComponent(i: number): string {
-        throw new Error("needs implementation");
+        this.checkIndex(i);
+        return this.components[i];
     }
 
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
+        this.checkIndex(i);
+        this.components[i] = c;
     }
 
+    // @methodtype get-method
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.components.length;
     }
 
+    // @methodtype command-method
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
+        this.checkIndex(i);
+        this.components.splice(i, 0, c);
     }
 
+    // @methodtype set-method
     public append(c: string): void {
-        throw new Error("needs implementation");
+        this.components.push(c);
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
-        throw new Error("needs implementation");
+        this.checkIndex(i);
+        this.components.splice(i, 1);
+    }
+
+    private checkIndex(i: number): void {
+        if (i < 0 || i >= this.getNoComponents()) {
+            throw new Error("Index out of bounds");
+        }
     }
 
 }
